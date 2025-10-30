7247e5e3e374632a64dac08bbbf1b602cd9725b0
redmor1 <lightz17@hotmail.com>
2025-10-30 18:06:22 -0300

    first commit

---
+++ Grupos.API/Entidades/Grupo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Grupos.API.Entidades
+{
+    public class Grupo
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        public ICollection<MiembroGrupo> Miembros { get; set; } = new List<MiembroGrupo>();
+
+    }
+}
---
+++ Grupos.API/Entidades/MiembroGrupo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Grupos.API.Entidades
+{
+    [PrimaryKey(nameof(GrupoId), nameof(UsuarioId))]
+    public class MiembroGrupo
+    {
+        // FK a la tabla Grupo
+        public Guid GrupoId { get; set; }
+        public Grupo Grupo { get; set; } // Propiedad de navegación
+
+        // FK al "Usuario".
+        // Nota: El usuario "vive" en Keycloak y en Usuarios.API.
+        // Aca solo guardamos su ID para saber que es miembro.
+        public Guid UsuarioId { get; set; }
+    }
+}
