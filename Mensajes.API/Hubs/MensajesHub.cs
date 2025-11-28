b72fd0325c5f7a192dad7dfb8e8b9592d5843dda
redmor1 <lightz17@hotmail.com>
2025-11-28 05:42:23 -0300

    Agregar mas metodos a MensajesHub

--- Mensajes.API/Hubs/MensajesHub.cs
+++ Mensajes.API/Hubs/MensajesHub.cs
@@ -13,5 +13,16 @@ namespace Mensajes.API.Hubs
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, conversacionId);
         }
+
+        public async Task Escribiendo(string conversacionId, string usuarioNombre)
+        {
+            // Enviar a todos en el grupo EXCEPTO al que está escribiendo
+            await Clients.GroupExcept(conversacionId, Context.ConnectionId).SendAsync("UsuarioEscribiendo", new { ConversacionId = conversacionId, Usuario = usuarioNombre });
+        }
+
+        public async Task DejoDeEscribir(string conversacionId, string usuarioNombre)
+        {
+            await Clients.GroupExcept(conversacionId, Context.ConnectionId).SendAsync("UsuarioDejoDeEscribir", new { ConversacionId = conversacionId, Usuario = usuarioNombre });
+        }
     }
 }
